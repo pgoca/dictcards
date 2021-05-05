50ceeb190d6824cdb9115bd95894ecd8dde15df1
Paweł Goca <pawel@goca.me>
2021-05-05 18:00:28 +0200

    Initial Commit

---
+++ dictcards/dictcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  dictcardsApp.swift
+//  dictcards
+//
+//  Created by Paweł Goca on 05/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct dictcardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
